4e8d09a8801f29979dc378db68c210cffbdf1323
mohammedash-iq <mohammedashiq206@gmail.com>
2025-08-04 21:54:15 +0530

    added middleware for cors, errors, logs and cookieparser

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
+const corsOptions = require("./config/corsOptioins");
+const cookieParser = require("cookie-parser");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
 
 app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser);
 
 app.use("/", require("./routes/root"));
 
-app.all("*", (req, res) => {
+app.all("/", (req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "notFound.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
